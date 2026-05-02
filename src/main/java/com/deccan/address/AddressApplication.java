2eeaa9f88b7c5eaa797578fe6c962d30507818fb
Ranjithkumar0203 <ranjith.kumar03@gmail.com>
2026-05-02 13:41:37 +0530

    main

---
+++ src/main/java/com/deccan/address/AddressApplication.java
@@ -0,0 +1,14 @@
+package com.deccan.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressApplication.class, args);
+		
+	}
+
+}
